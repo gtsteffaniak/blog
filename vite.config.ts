9f2c42f094fc043b0e333073cdf164da33560427
Graham Steffaniak <graham.steffaniak@autodesk.com>
2023-12-21 14:28:08 -0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   plugins: [svelte()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // Change this value as needed
+  },
+});
